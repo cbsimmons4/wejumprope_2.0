3d70b57f9ec109f1e44d75acce33165d07da6837
Cameron Simmons <cbsimmons4@gmail.com>
2021-09-11 23:50:28 -0400

    ian henry

--- production/src/components/Sections/AboutSection.js
+++ production/src/components/Sections/AboutSection.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import TJImage from '../assets/devTJtransparent.png';
+import crypticImage from '../assets/crypticimage.png';
 import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,7 +16,8 @@ const useStyles = makeStyles((theme) => ({
   image: {
       width: '100%',
       maxWidth: 400,
-      margin: 'auto'
+      margin: 'auto',
+      cursor: 'pointer'
   },
   aboutText1: {
       padding: 20,
@@ -32,6 +37,31 @@ button: {
     width: 300,
     marginBottom: 100,
     background: 'radial-gradient(circle, rgba(251,63,63,1) 0%, rgba(158,35,35,1) 78%, rgba(96,17,17,1) 100%)',
+},
+modalWrapper: {
+    // width: '100%',
+    // height: '100%',
+    
+},
+crpyWrapper: {
+    width: '100%'
+},
+crpyimage: {
+    display: 'block',
+    margin: 'auto',
+    width: '80%'
+},
+crypticMessageWapper: {
+    width: '80%',
+    margin: 'auto',
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    padding: '10px'
+},
+inputField: {
+    backgroundColor: '#FFF'
+},
+submitButton: {
+    marginTop: '20px'
 }
   
 }));
@@ -39,13 +69,39 @@ button: {
 export default function AboutSection() {
 
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
+    const [isIan, setisIan] = useState(false);
+    const [input, setInput] = useState('');
+
+    const itIshandleIsIan = () => {
+        setisIan(true);
+    }
+
+    const itIsNOThandleIsIan = () => {
+        setisIan(false);
+    }
+
+    const handleOpen = () => {
+        setOpen(true);
+      };
+    
+    const handleClose = () => {
+        setOpen(false);
+        itIsNOThandleIsIan();
+    };
+
+    const handleSubmit = (answer) => {
+        if (answer.toLowerCase().trim() === 'gregnorf') {
+            itIshandleIsIan();
+        }
+    }
 
     return (
     <div className={classes.root}>
         <h1>About</h1>
         <Grid container spacing={3}>
             <Grid item sm={12} md={4}>
-            <img src={TJImage} alt='' className = {classes.image}/>
+                    <img src={TJImage} alt='' className = {classes.image} onClick = {handleOpen}/>                
             </Grid>
             <Grid item sm={12} md={8}>
                 <p className = {classes.aboutText1}>
@@ -66,6 +122,40 @@ export default function AboutSection() {
                 </Button>
             </Grid>
         </Grid>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        >
+            <div className = {classes.crpyWrapper}>
+                <ClickAwayListener onClickAway={handleClose}>
+                    {!isIan ? 
+                        <div className={classes.crypticMessageWapper}>
+                            <>
+                                <h1>SUPER HIGH-TECH SECURITY THAT ONLY ALLOWS IAN HENRY TO ACCESS</h1>
+                                <p>Type in the name of the great captain who had many great pear adventures to continue.<br/><br/>CAPTAIN...</p>
+                                <TextField
+                                className={classes.inputField}
+                                id="standard-basic"
+                                placeholder = 'enter here' 
+                                value = {input}
+                                onChange = { (event) => {setInput(event.target.value)}} />
+                                <div>
+                                <Button className={classes.submitButton} variant="contained" color="primary" onClick = {() => {handleSubmit(input)}}>
+                                    Submit
+                                </Button>
+                                </div>
+                             </>
+                        </div>
+                    : 
+                    <div className={classes.crypticMessageWapper}>
+                        <p>Access Granted. Hello Ian Henry...</p>
+                        <p>Here is your next clue.</p>
+                        <img src={crypticImage} alt='' className = {classes.crpyimage} onClick = {handleOpen}/>
+                    </div>
+                    }
+                </ClickAwayListener>
+            </div>
+        </Modal>
     </div>
     );
 }
